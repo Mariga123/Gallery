c62ede7d14de8d8240e4aa8b2309cf4e8de730f4
Mariga123 <johnmariga8@gmail.com>
2021-01-10 19:16:13 +0300

    add save and delete methods for image models

--- photos/models.py
+++ photos/models.py
@@ -8,6 +8,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()  
  
 
 class Location(models.Model):
@@ -21,6 +27,15 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
