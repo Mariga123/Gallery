b1fd4381f44b359f7cbd4d638bc5bf0e020aa958
Mariga123 <johnmariga8@gmail.com>
2021-01-10 15:01:50 +0300

    create image model and load media settings

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
