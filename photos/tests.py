a2202a9bf1f858c8bf9074e98eb2ce431b50c55f
Mariga123 <johnmariga8@gmail.com>
2021-01-11 09:47:54 +0300

    create test for updating location

--- photos/tests.py
+++ photos/tests.py
@@ -52,4 +52,34 @@ class CategoryTestClass(TestCase):
     def test_delete_category(self):
         self.category.delete_category()
         category = Category.objects.all()
-        self.assertTrue(len(category) == 0)        
+        self.assertTrue(len(category) == 0)      
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location = Location(name='Moringa')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_update_location(self):
+        new_location = 'kericho'
+        self.location.update_location(self.location.id, new_location)
+        changed_location = Location.objects.filter(name='kericho')
+        self.assertTrue(len(changed_location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+  
