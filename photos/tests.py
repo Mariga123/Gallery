eb13bb59da20dd5e7e307624132768ab719beee2
Mariga123 <johnmariga8@gmail.com>
2021-01-11 09:44:42 +0300

    create save and delete methods for images and run test

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='town')
+        self.location.save_location()
+
+        self.category = Category(name='food')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='my best meal', location=self.location,
+                                category=self.category)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image)) 
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)                               
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)   
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id, 'media/test.jpg')
+        changed_img = Image.objects.filter(image='media/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image, image) 
